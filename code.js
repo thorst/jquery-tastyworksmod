b9d048649cee6d4dd1a73ebeb63b633f6495cbb7
Todd Horst <toddmhorst@yahoo.com>
2019-04-18 12:20:18 -0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,1 @@
+javascript:(function()%7B%24(".page-sidebar").remove();%24(".dough-wrapper").css(%7Bmargin%3A0%2Cwidth%3A"100%25"%7D);window.dispatchEvent(new Event("resize"));%24("a[href='#/homePage'],a[href='#/followPage'],a[href='#/journalPage'],a[href='#/tastytradePage']").remove();%7D)()
